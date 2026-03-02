c0d355250751ab8d551fabef59826989095e6987
netfilmes52-alt <netfilmes52@gmail.com>
2026-03-01 21:02:42 -0300

    Create command handling system for Fogo Amigo addon

---
+++ behavior_packs/fogo-amigo/scripts/commands.js
@@ -0,0 +1,8 @@
+// Command handling system for the Fogo Amigo addon
+
+function handleCommand(command) {
+    // Implement command handling logic here
+}
+
+// Export the command handler
+module.exports = { handleCommand };
